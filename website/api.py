eef57ffe7a2326cadaa1c940469484664f8b3721
Armon <armon@nudgex.net>
2024-04-18 15:23:54 -0700

    started setting up endpoint for PlateLoc

--- website/api.py
+++ website/api.py
@@ -4,8 +4,13 @@ from . import Core, Engine, Procedure, System
 
 api = Blueprint('api', __name__, url_prefix='/api')
 path_to_pvar_file = r"C:\Users\chris\Desktop\overlord_variables\release_variables.pvar"
-@api.route('/run-sequence',methods=['POST'])
-def run_sequence():
+
+@api.route('/move-plate',methods=['POST'])
+def move_plate():
+    """
+    Either Get from a location or Put to it
+    Currently supporting HotelA, Timepoint_collector, and PlateLoc
+    """
     pvar_dict = request.json['kwargs']
     pvar_str = dict_to_pvar_string(pvar_dict)
     with open(path_to_pvar_file,'w') as f:
@@ -15,7 +20,22 @@ def run_sequence():
     try:
         proc_info = System.IO.FileInfo(r"C:\Program Files (x86)\PAA\Overlord3\Procedures\Testing\PMAN_Move.ovp")
     except:
-        raise FileNotFoundError("Can't find the ovp!")
+        raise FileNotFoundError("Can't find the PMAN_Move ovp!")
+    procedure = Procedure(proc_info)
+    engine.ProcedureComplete += engine_procedure_complete
+    engine.Start(procedure)
+    return "Sequence started!"
+
+@api.route('seal-plate',methods=['POST'])
+def seal_plate():
+    """ This seals whatever plate is on PlateLoc """
+    Core.Commands.RuntimeInitialize()
+    engine = Engine()
+    try:
+        # TODO: link the plate-seal ovp
+        proc_info = System.IO.FileInfo(r"C:\Not\A\Real\File\Path\PlateLoc.ovp")
+    except:
+        raise FileNotFoundError("Can't find the PlateLoc ovp!")
     procedure = Procedure(proc_info)
     engine.ProcedureComplete += engine_procedure_complete
     engine.Start(procedure)
